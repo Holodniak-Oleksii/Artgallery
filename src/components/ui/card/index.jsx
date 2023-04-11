b9508473886a6cc66f761f74208f1f9c27bb6272
Oleksii Holodniak <holodniakwork@gmail.com>
2023-04-11 18:16:41 +0300

    card fix

--- src/components/ui/card/index.jsx
+++ src/components/ui/card/index.jsx
@@ -3,11 +3,27 @@ import { Wrapper, InformationBar, Flex } from './style';
 import { BlueButton } from '@/components/ui';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
+import { useMediaQuery } from 'react-responsive';
 
 const CardModel = ({ data }) => {
   const route = useRouter();
+  const isTablet = useMediaQuery({ maxWidth: 1024 });
+
+  const handlerPush = () => {
+    if (data._id) {
+      route.push(`/details/${data._id}`);
+    } else route.push(`/spaces?category=all&search=`);
+  };
+
   return (
-    <Wrapper className="card">
+    <Wrapper
+      className="card"
+      onClick={() => {
+        if (isTablet) {
+          handlerPush();
+        }
+      }}
+    >
       <Image
         src={data.image || data.pathImage}
         alt="model"
@@ -21,11 +37,7 @@ const CardModel = ({ data }) => {
           <span className="author">@{data.ownerName}</span>
           <BlueButton
             outline
-            onClick={() => {
-              if (data._id) {
-                route.push(`/details/${data._id}`);
-              } else route.push(`/spaces?category=all&search=`);
-            }}
+            onClick={handlerPush}
           >
             View
           </BlueButton>
